c033de2c842895366240e47a73e7aa85a00cd6fb
mpatel89 <lilmp89@gmail.com>
2015-08-17 23:04:22 -0500

    Add read data functions

--- plot1.R
+++ plot1.R
@@ -1,1 +1,10 @@
 #plot1.R
+#set classes for columns; mainly for dates as characters
+colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+
+#read header and data with proper head names and select rows
+colNamesVec <- as.character(read.table("household_power_consumption.txt", sep = ";", colClasses = "character", nrows = 1))
+x <- read.table("household_power_consumption.txt", sep = ";", col.names=colNamesVec, colClasses = colClasses, skip =66636, nrows = 2880)
+
+#change Date and Times to respective formats.
+# as.Date(x[[1]][1], format="%d/%m/%Y")
