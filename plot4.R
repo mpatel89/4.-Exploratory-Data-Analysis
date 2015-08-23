a3ef959d81d74772f2c5d8fe4d04f0b6d2315bc8
mpatel89 <lilmp89@gmail.com>
2015-08-22 22:30:35 -0500

    Adds plot3.R, plot4.R, plot1.png, plot2.png, plot3.png, and plot4.png

---
+++ plot4.R
@@ -0,0 +1,37 @@
+#plot1.R
+
+#sets working directory
+setwd("~/R/Coursera/4. Exploratory Data Analysis")
+
+#looks for data file or downloads if file not found
+if(!file.exists("household_power_consumption.txt"))
+{
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "data.zip")
+  unzip("data.zip")
+}
+
+#We want to select for Feb. 1st through Feb 2nd.The data starts on Dec. 16 of the previous year, so there are 46 days worth of reads to skip.
+#Also, to address the time of the start (6 hours, 36 minutes), which is 396 reads. 
+#That's 46 days * 24 hours * 60 minutes + 396 reads= 66636. This is how many we want to skip.
+#We want to go from 0:00:00 hour to 0:00:00 hour of Feb 3rd (bc the plots have Friday on them; we'll need that). That is 2881 reads for each minute.
+
+#read header and desired subset of data with proper head names
+colNamesVec <- as.character(read.table("household_power_consumption.txt", sep = ";", colClasses = "character", nrows = 1))
+x <- read.table("household_power_consumption.txt", sep = ";", col.names=colNamesVec, skip =66637, nrows = 2881)
+
+#change Date and Times to respective formats.
+x$DateTime <- as.POSIXct(paste(x$Date, x$Time), format = "%d/%m/%Y %H:%M:%S")  
+
+#Generate labeled histogram for Global Active Power frequency
+png("plot4.png")
+par(mfrow=c(2,2))
+with(x, {
+  plot(x$Global_active_power~x$DateTime, xlab = "", ylab = "Global Active Power", type = "l")
+  plot(x$Voltage~x$DateTime, xlab = "datetime", ylab = "Voltage", type = "l")
+  plot(x$Sub_metering_1 ~ x$DateTime, xlab = "", ylab = "Energy sub metering", type = "l")
+  points(x$Sub_metering_2 ~ x$DateTime,type = "l", col ="red")
+  points(x$Sub_metering_3 ~ x$DateTime, type = "l", col ="blue")
+  legend("topright", legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=c(1,1))
+  plot(x$Global_reactive_power~x$DateTime, xlab = "datetime", ylab = "Global_reactive_power", type = "l")
+})
+dev.off()
